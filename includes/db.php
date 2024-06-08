41ee833c5d5d080af51b72aac1b471bf978a9b9a
Ngatour Rohman <ngatourrohman25@gmail.com>
2024-06-08 14:58:44 +0700

    update conn

--- includes/db.php
+++ includes/db.php
@@ -2,10 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "libraryadmin";
+$dbname = "library";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Memeriksa koneksi
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
