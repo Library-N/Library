6f80016f526da9a3b8dab3ee26fb9b5b9eba2bec
sabdha04 <sabdhacipaku04@gmail.com>
2024-06-29 21:23:09 +0700

    improvement/members: memperbaharui tampilan members

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
+    <title>Dashboard</title>
     <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="./css/style.css">
     <script>
@@ -22,82 +22,9 @@
 
 <body>
     <!-- =============== Navigation ================ -->
-    <div class="container">
-        <div class="navigation">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/Ellipse 16.svg" alt="logo">
-                        </span>
-                        <span class="title">Library - N</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/ic_round-home.svg" alt="home_icon">
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/fa_book.svg" alt="book_icon">
-                        </span>
-                        <span class="title">Books</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/f7_person-2-fill.svg" alt="member_icon">
-                        </span>
-                        <span class="title">Members</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/lets-icons_date-range-fill.svg" alt="loan_icon">
-                        </span>
-                        <span class="title">Loans</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/material-symbols_library-books.svg" alt="report_icon">
-                        </span>
-                        <span class="title">Report</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="./auth/logout.php">
-                        <span class="icon">
-                            <img src="assets/icons/majesticons_logout.svg" alt="logout_icon">
-                        </span>
-                        <span class="title">Log out</span>
-                    </a>
-                </li>
-                <li class="settings">
-                    <a href="#">
-                        <span class="icon">
-                            <img src="assets/icons/uiw_setting.svg" alt="setting_icon">
-                        </span>
-                        <span class="title">Settings</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?php
+    include './navbar.php';
+    ?>
     <!-- ========================= Main ==================== -->
     <div class="main">
         <div class="topbar">
@@ -105,11 +32,12 @@
                 <img src="assets/icons/pepicons-pop_menu.svg" alt="menu">
             </div>
         </div>
+
         <div>
             <?php
             session_start();
             if (!isset($_SESSION['user_id'])) {
-                header("Location: index1.php");
+                header("Location: ./auth/login.php");
                 exit();
             }
 
@@ -125,9 +53,9 @@
                 </div>
             <?php endif; ?>
         </div>
+
         <!-- =========== Scripts =========  -->
         <script src="js/main.js"></script>
-
 </body>
 
 </html>
--- loans.php
+++ loans.php
@@ -1,11 +1,34 @@
-<?php
-include 'includes/header.php';
-?>
-
-<h2>Loans Management</h2>
-<p>Manage all the loans of books from here.</p>
-
-</main>
+<?php include('./includes/db.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loans</title>
+    <!-- ======= Styles ====== -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/loans.css">
+</head>
+<body>
+    <!-- =============== Navigation ================ -->
+    <?php include './navbar.php'; ?>
+    <!-- ========================= Main ==================== -->
+    <div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <img src="assets/icons/pepicons-pop_menu.svg" alt="menu">
+            </div>
+            <h2>Loans</h2>
+            <div class="tambahbtn">
+                <a href="addMember.php"><i class="fa fa-plus"></i><b> Add Loans</b></a>
+            </div>
+        </div>
+        <div class="btnActiveLoans">
+                <a href="addMember.php"><b> Active Loans</b></a>
+            </div>
+        <!-- =========== Scripts =========  -->
+        <script src="js/main.js"></script>
+    </div>
 </body>
-
 </html>
