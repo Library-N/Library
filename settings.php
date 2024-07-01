4e7a7b289e6fbfcf6e045be6942e762e82993b89
Ngatour Rohman <ngatourrohman25@gmail.com>
2024-07-02 03:14:11 +0700

    fix settings

--- settings.php
+++ settings.php
@@ -1,3 +1,27 @@
-<?php
-    include 'includes/navbar.php';
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Settings</title>
+    <link rel="stylesheet" href="css/books.css">
+</head>
+
+<body>
+    <!-- =============== Navigation ================ -->
+    <?php include 'includes/navbar.php'; ?>
+
+    <!-- ========================= Main ==================== -->
+    <div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <img src="assets/icons/pepicons-pop_menu.svg" alt="menu">
+            </div>
+        </div>
+        <!-- =========== Scripts =========  -->
+        <script src="js/main.js"></script>
+    </div>
+</body>
+
+</html>
